385b19831938b93b98a2c05fef8ecc671e4f5a47
Carson McNeal <carsonmcneal14@gmail.com>
2024-10-07 09:25:46 -0500

    game menu

---
+++ PAs/PA3/PA3.cs
@@ -0,0 +1,24 @@
+//PA3  main start
+using System;
+namespace CatCade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+Console.WriteLine(" ================");
+Console.WriteLine("  GAME SELECTION ");
+Console.WriteLine(" ================");
+Console.Clear();
+Console.WriteLine("1. Slot Machine");
+Console.WriteLine("2. Hide & Seek");
+Console.WriteLine("3. BlackJack");
+Console.WriteLine("4. Exit");
+
+string input = Console.ReadLine();
+            }
+        }
+    }
+}
